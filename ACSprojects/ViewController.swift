bd8a9cf36fccb6da8498235fc4e1572b11d9ec28
Максим Евлентьев <maksimevlentev@macbook-air-maksim.local>
2016-12-22 00:32:59 +0500

    Initial Commit

---
+++ ACSprojects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ACSprojects
+//
+//  Created by Максим Евлентьев on 22.12.16.
+//  Copyright © 2016 Максим Евлентьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
